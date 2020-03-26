7b3bc9e9ad51ec303f8bba3f9ff90b2d7cb114c2
Felipe <felipe_londono1@hotmail.com>
2020-03-26 02:48:07 +0000

    [Add]: added comment

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,3 +1,4 @@
+#script to setup a server with puppet
 exec { 'installNginx':
   command  =>  'sudo apt-get -y update ; sudo apt-get -y install nginx ; echo \"Holberton School\" | sudo tee /var/www/html/index.nginx-debian.html ;  sudo sed -i "s/server_name _;/server_name _;\n\trewrite ^\/redirect_me https:\/\/www.youtube.com\/watch?v=QH2-TGUlwu4 permanent;/" /etc/nginx/sites-available/default ; sudo service nginx start',
   provider =>  shell,
