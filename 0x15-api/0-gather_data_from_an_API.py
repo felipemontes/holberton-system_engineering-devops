97e8f287c78b6b9c66d780a084cca95e6d69d877
Felipe <felipe_londono1@hotmail.com>
2020-05-04 15:48:37 +0000

    [Add]: gather employee info from a REST API

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" python script to use a rest api """
+import requests
+from sys import argv as a
+
+if __name__ == '__main__':
+    users = 'https://jsonplaceholder.typicode.com/users/{}'.format(a[1])
+    todos = 'https://jsonplaceholder.typicode.com/todos?userId={}'.format(a[1])
+    task = 0
+    done = 0
+    user = requests.get(users).json()
+    name = user.get('name')
+    c_tasks = []
+
+    to_do = requests.get(todos).json()
+    for dic in to_do:
+        task += 1
+        if dic.get('completed') is True:
+            done += 1
+            c_tasks.append(dic.get('title'))
+
+    print("Employee {} is done with tasks ({}/{})".format(name, done, task))
+    for title in c_tasks:
+        print("     {}".format(title))
