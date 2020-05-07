c6852bc5f77c0201cbbbf306c152f41d63c4372f
Felipe <felipe_londono1@hotmail.com>
2020-05-07 23:10:44 +0000

    [Add]: get reddit titles using recursion

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+""" request all the titles of a subreddit"""
+import requests
+
+
+def recurse(subreddit, hot_list=[], after=None):
+    """ recursive function to get all titles """
+
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    headers = {
+        'User-Agent': '1.0'
+    }
+    params = {
+        'limit': 100,
+        'after': after
+    }
+    response = requests.get(url, allow_redirects=False, headers=headers,
+                            params=params)
+    r = response.json()
+
+    if response:
+        x = r.get('data').get('children')
+        aft_val = r.get('data').get('after')
+        for i in x:
+            hot_list.append(i.get('data').get('title'))
+        if not aft_val:
+            return (hot_list)
+        return (recurse(subreddit, hot_list, aft_val))
+    else:
+        print(None)
